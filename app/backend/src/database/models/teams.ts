b2103be2fa91763b09e5eda43c23f8b485e9c989
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2022-06-06 11:17:37 -0300

    feat: create migration and model matches

--- app/backend/src/database/models/teams.ts
+++ app/backend/src/database/models/teams.ts
@@ -1,5 +1,6 @@
 import { Model, DataTypes } from 'sequelize';
 import db from '.';
+import matches from './matches';
 
 class teams extends Model {
   teamName: string;
@@ -11,6 +12,24 @@ teams.init({
   underscored: true,
   modelName: 'teams',
   timestamps: false,
+});
+
+matches.belongsTo(teams, {
+  foreignKey: 'id',
+  as: 'teams_home_team' 
+});
+matches.belongsTo(teams, {
+  foreignKey: 'id',
+  as: 'teams_away_team'
+});
+
+teams.hasMany(matches, {
+  foreignKey: 'home_team',
+  as: 'matches_home_team'
+});
+teams.hasMany(matches, {
+  foreignKey: 'away_team',
+  as: 'matches_away_team'
 });
 
 export default teams;
