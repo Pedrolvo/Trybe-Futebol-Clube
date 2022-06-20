31de1245e73216a8efde0f56957612266f0582c6
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2022-06-20 13:36:08 -0300

    feat: adding validation in controller of login

--- app/backend/src/database/controllers/login.ts
+++ app/backend/src/database/controllers/login.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { nextTick } from 'process';
 import loginService from '../services/login';
 
 class LoginController {
@@ -14,6 +15,19 @@ class LoginController {
       return res.status(200).json(sign);
     } catch (err) {
       next (err);
+    }
+  };
+
+  validation = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { authorization } = req.headers;
+
+      const sign = await loginService.validation(authorization as string);
+      if (!sign) return res.status(401).json({ message: 'Invalid Token' });
+
+      return res.status(200).json(sign);
+    } catch (err) {
+      next(err);
     }
   };
 }
