926c7df6480392c892b4943f85b087c66dfa536a
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2022-06-21 12:01:23 -0300

    feat: add route /:id for matches

--- app/backend/src/database/routes/matches.ts
+++ app/backend/src/database/routes/matches.ts
@@ -7,5 +7,6 @@ const routerMatch = express.Router();
 routerMatch.get('/', MatchControllers.getByProgress);
 routerMatch.post('/', MatchControllers.createMatch);
 routerMatch.patch('/:id/finish', MatchControllers.patchMatch);
+routerMatch.patch('/:id', MatchControllers.patchById);
 
 export default routerMatch;
