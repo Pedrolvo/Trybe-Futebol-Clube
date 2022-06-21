8f3237de22ddd81bdc5baccc4089068b547c4805
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2022-06-21 11:32:08 -0300

    feat: add createMatch and patchMatch in controller matches

--- app/backend/src/database/controllers/matches.ts
+++ app/backend/src/database/controllers/matches.ts
@@ -8,8 +8,36 @@ class MatchControllers {
       const boolean = inProgress === 'true';
       const result = inProgress ? boolean : undefined;
       const games = await MatchServices.getByProgress(result);
-      
+
       return res.status(200).json(games);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  createMatch = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { authorization } = req.headers;
+      const { homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress } = req.body;
+
+      const newMatch = await MatchServices.createMatch({
+        homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress }, authorization as string);
+      return res.status(201).json(newMatch);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  patchMatch = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+
+      const pkPatch = await MatchServices.patchMatch(Number(id));
+      if (!pkPatch) {
+        return res.status(404).json({ message: 'ERROR' });
+      }
+
+      return res.status(200).json({ message: 'Finished' });
     } catch (err) {
       next(err);
     }
