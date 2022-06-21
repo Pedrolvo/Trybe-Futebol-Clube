0dc4000074156669fb8dde5dad20f6454f915c84
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2022-06-21 11:35:25 -0300

    feat: add post and patch routes in matches

--- app/backend/src/database/routes/matches.ts
+++ app/backend/src/database/routes/matches.ts
@@ -5,5 +5,7 @@ import MatchControllers from '../controllers/matches';
 const routerMatch = express.Router();
 
 routerMatch.get('/', MatchControllers.getByProgress);
+routerMatch.post('/', MatchControllers.createMatch);
+routerMatch.patch('/:id/finish', MatchControllers.patchMatch);
 
 export default routerMatch;
