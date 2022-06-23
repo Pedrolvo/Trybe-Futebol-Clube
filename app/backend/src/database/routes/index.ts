b66e3648818332549cc6225d8911edd55c846deb
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2022-06-23 17:05:01 -0300

    feat: and leaderBoard in routes

--- app/backend/src/database/routes/index.ts
+++ app/backend/src/database/routes/index.ts
@@ -1,4 +1,4 @@
-import * as express from 'express';
+import express from 'express';
 
 import routerLogin from './login';
 import routerTeam from './teams';
--- app/backend/src/database/routes/leaderBoards.ts
+++ app/backend/src/database/routes/leaderBoards.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import LeaderBoardContollers from '../controllers/leaderBoardHome';
+import LeaderBoardControllers from '../controllers/leaderBoardAway';
 
 const routerLeaderBoard = express.Router();
 
 routerLeaderBoard.get('/home', LeaderBoardContollers.getAll);
+routerLeaderBoard.get('/away', LeaderBoardControllers.getAll);
 
 export default routerLeaderBoard;
